6a15aa6dc1aa7cd71f0b4ea95085623897d13778
김근호 <kimkgh8@pinnode.com>
2024-01-03 01:29:44 +0900

    ✨ Feat: 초기 프로젝트 생성

---
+++ src/test/java/com/dev/kit/functions/FunctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.kit.functions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
